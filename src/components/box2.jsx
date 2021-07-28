4b5af02d2b87ad8d74be5cb1821eca11c0f3f5c8
chenshenhai <cshenhai@gmail.com>
2021-07-29 01:13:24 +0800

    feat: update

---
+++ src/components/box2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './box2.less';
+
+export default function Box2() {
+  return (
+    <div className="box2">
+      Box Component
+    </div>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import loadable from '@loadable/component'
 import './index.less';
 
-const Box = loadable(() => import('./components/box'));
+const Box = loadable(() => {
+  const promise = import('./components/box');
+  return promise;
+});
+
+const lazyLoad = (dymanicImport) => {
+  return function LazyComponent() {
+    const ref = useRef(null)
+    useEffect(() => {
+      dymanicImport().then((mod) => {
+        let Module = null
+        if (mod.__esModule === true && mod.default) {
+          Module = mod.default;
+        } else {
+          Module = mod;
+        }
+        ReactDOM.render(React.createElement(
+          Module, null, {}
+        ), ref.current)
+      }).catch(console.log);
+    }, []);
+
+    return (
+      <div ref={ref}></div>
+    );
+  }
+}
+
+
+
+
+const Box2 = lazyLoad(() => import('./components/box2'));
 
 function App() {
   const [showBox, setShowBox] = useState(false);
@@ -18,6 +49,7 @@ function App() {
     <div className="app">
       {showBox === true && <Box />}
       <div>Hello World</div>
+      <Box2 />
     </div>
   )
 }
