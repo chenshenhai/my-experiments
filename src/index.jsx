ac6ee90294699885800fbeea51f5e2e07d7be0da
chenshenhai <cshenhai@gmail.com>
2021-07-29 00:33:05 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import loadable from '@loadable/component'
 import './index.less';
 
-const Box = loadable(() => import('./components/box'))
+const Box = loadable(() => import('./components/box'));
+
+function App() {
+  const [showBox, setShowBox] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowBox(true);
+    }, 3000)
+  }, []);
+
+  return (
+    <div className="app">
+      {showBox === true && <Box />}
+      <div>Hello World</div>
+    </div>
+  )
+}
 
 ReactDOM.render((
-  <div className="app">
-    <Box />
-    <div>Hello World</div>
-  </div>
+  <App />
 ), document.querySelector('#app'));
