8f40169c6cf9bd43a2dc4b663aac63dfa451c553
chenshenhai <cshenhai@gmail.com>
2021-07-28 22:52:38 +0800

    update

---
+++ scripts/webpack.prod.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const ENV = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+
+const fileResolve = function (file) {
+  return path.join(__dirname, '..', file);
+};
+
+module.exports = {
+  mode: ENV, 
+  devtool: 'inline-cheap-module-source-map',  
+ 
+  entry: {
+    'index' : fileResolve('src/index.jsx'),
+  },
+ 
+  output: {
+    path: fileResolve(''),
+    filename: 'dist/[name].js',
+  },
+  module: { 
+    rules: [ 
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            'plugins': []
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js' ],
+  },
+  plugins: [],
+
+  devServer: {
+    contentBase: path.join(__dirname, '..'),
+    port: 9000,
+    hot: false,
+    inline: false,
+  }
+}
