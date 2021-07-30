53b76ffd73ac58aea7b5f37613d8100c9d472dcd
chenshenhai <cshenhai@gmail.com>
2021-07-30 17:54:21 +0800

    update

--- src/data.js
+++ src/data.js
@@ -4,6 +4,7 @@ main();
 
 async function main() {
   await insertData();
+  await insertList();
 }
 
 
@@ -16,6 +17,22 @@ async function insertData() {
     password: Math.random().toString(36).substr(2),
   }
   const result = await doConnect(sql, values);
+  if (result.insertId > 0) {
+    console.log('insert table success!');
+  } else {
+    console.log('insert table fail!');
+  }
+}
+
+
+async function insertList() {
+  const sql = `
+    INSERT INTO \`user_info\` (name, password) VALUES ?;
+  `;
+  const values =  [0, 1, 2, 3, 4].map((i) => {
+    return [`Hello${i}`, Math.random().toString(36).substr(2)]
+  })
+  const result = await doConnect(sql, [values]);
   if (result.insertId > 0) {
     console.log('insert table success!');
   } else {
--- src/table.js
+++ src/table.js
@@ -21,19 +21,3 @@ async function createTable() {
   console.log(result);
   console.log('create table success!');
 }
-
-async function insertTable() {
-  const sql = `
-    INSERT INTO \`user_info\` SET ?;
-  `;
-  const pageId = randomStr(8);
-  const result = await doConnect(sql, {
-    page_id: pageId,
-    config: JSON.stringify(params.config || {}),
-  });
-  if (result.insertId > 0) {
-    console.log('insert table success!');
-  } else {
-    console.log('insert table fail!');
-  }
-}
