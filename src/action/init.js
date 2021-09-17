462c30a4d9aea2368733fdb41c8d6cd5029a99bf
chenshenhai <cshenhai@gmail.com>
2021-09-17 19:54:38 +0800

    feat: update

--- src/action/init.js
+++ src/action/init.js
@@ -2,12 +2,21 @@ const inquirer = require('inquirer');
 
 module.exports = async function init(targetDir, options) {
 
-  const { name, version } = await inquirer.prompt([
+  const { name, template } = await inquirer.prompt([
+    {
+      name: 'template',
+      type: 'list',
+      choices: ['demo1', 'demo2'],
+      message: `Please select template: `
+    },
     {
       name: 'name',
       type: 'input',
+      default: 'helloworld',
       message: `Please input name: `
     },
   ])
-  console.log('data =', { name, version });
+
+
+  console.log('data =', { name, template });
 }
