6df70ebad6b4b3efb98acc7effb5941927218c1a
chenshenhai <cshenhai@gmail.com>
2021-09-17 16:27:55 +0800

    feat: update

--- src/ask.js
+++ src/ask.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-async function ask(options) {
+module.exports = async function ask(options) {
   
   const { name, version } = await inquirer.prompt([
     {
@@ -16,13 +16,4 @@ async function ask(options) {
   ])
 
   console.log('data =', { name, version });
-}
-
-module.exports = (...args) => {
-  return ask(...args).catch(err => {
-    console.log(err);
-    if (!process.env.VUE_CLI_TEST) {
-      process.exit(1)
-    }
-  })
 }
