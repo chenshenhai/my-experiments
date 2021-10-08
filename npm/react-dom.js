bc0c115a7441e9baf970268690d55462998b6f27
chenshenhai <cshenhai@gmail.com>
2021-10-08 13:19:15 +0800

    feat: update

---
+++ npm/react-dom.js
@@ -0,0 +1,3 @@
+import './../node_modules/react-dom/umd/react-dom.development.js';
+
+export default window.ReactDOM;
---
+++ npm/react.js
@@ -0,0 +1,3 @@
+import './../node_modules/react/umd/react.development.js';
+
+export default window.React;
