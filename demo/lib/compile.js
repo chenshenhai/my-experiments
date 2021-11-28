6f58ee091373f910941f9452f601da7c3ef529b1
chenshenhai <cshenhai@gmail.com>
2021-11-28 16:35:40 +0800

    feat: update

--- demo/lib/compile.js
+++ demo/lib/compile.js
@@ -5,17 +5,24 @@ export function compile(tpl) {
   codeLines.push('var _line_ = [];')
   codeLines.push('with(data) {')
   while (tpl) {
-    let start = tpl.indexOf('<%')
-    const end = tpl.indexOf('%>')
+    let start = tpl.indexOf('{{')
+    const end = tpl.indexOf('}}')
     if (start > -1 && end > -1) {
       if (start > 0) {
         codeLines.push('_line_.push(' + JSON.stringify(tpl.slice(0, start)) + ');')
       }
-      if (tpl.charAt(start + 2) === '=') {
-        codeLines.push('_line_.push(' + tpl.slice(start + 3, end) + ');')
+      let script = tpl.slice(start + 2, end);
+      if (/(\$for|\$if)/.test(script)) {
+        script = script + ' {';
+        script = script.replace(/\$/ig, '')
+        codeLines.push(script)
+      } else if (/\$end/.test(script)) {
+        script = script.replace(/\$end/ig, '}')
+        codeLines.push(script)
       } else {
-        codeLines.push(tpl.slice(start + 2, end))
+        codeLines.push('_line_.push(' + script + ');')
       }
+
       tpl = tpl.slice(end + 2)
     } else {
       codeLines.push('_line_.push(' + JSON.stringify(tpl) + ');')
@@ -23,6 +30,6 @@ export function compile(tpl) {
     }
   }
   codeLines.push('}')
-  codeLines.push('codeLinesurn _line_.join("")')
+  codeLines.push('return _line_.join("")');
   return new Function('data', codeLines.join('\n'))
 };
