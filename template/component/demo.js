8ed6fd47fda187eccf3d69b818f58177492032af
chenshenhai <cshenhai@gmail.com>
2021-12-25 16:31:03 +0800

    feat: update

---
+++ template/component/demo.js
@@ -0,0 +1,20 @@
+Page({
+
+  data: {
+    title: '这是一个测试组件',
+    subTitle: 'Hello Component',
+    list: [0, 1, 2, 3, 4, 5]
+  },
+
+  onReady() {
+    console.log('onReady...')
+  },
+
+  onLoad() {
+    console.log('onLoad...')
+  },
+
+  myClickAction: function() {
+    this.setData({ msg: "Hello World" })
+  }
+})
