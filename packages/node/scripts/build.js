7f90025dd4f2e603b0a158515aef9832b64a9fba
chenshenhai <cshenhai@gmail.com>
2022-01-05 23:46:10 +0800

    feat: update

--- packages/node/scripts/build.js
+++ packages/node/scripts/build.js
@@ -8,13 +8,17 @@
 
 import babelParser from '@babel/parser';
 import { readJSX, writeDistFile } from './file.js';
+import { clearBabelAst } from './babel.js';
 
-const jsx = readJSX('Module.jsx');
-const ast = babelParser.parse(jsx, {
+const jsx = readJSX('Module.js');
+let ast = babelParser.parse(jsx, {
   plugins: [
     "jsx",
   ],
-})
+});
+
+clearBabelAst(ast)
+
 writeDistFile('jsx/Module.json', JSON.stringify(ast, null, 2))
 console.log(ast)
 
