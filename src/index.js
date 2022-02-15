0a1fae72f2ec1c5430992662169a5ce0e47c13f6
chenshenhai <cshenhai@gmail.com>
2022-02-15 23:19:37 +0800

    feat: update

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const heapdump = require('heapdump');
+
+const myTestCache = {};
+
+function setCache() {
+  myTestCache[`Hello_${Date.now()}`] = new Array(1e7);
+  const mem = process.memoryUsage().heapUsed / 1024 / 1024;
+  console.log('mem(MB) =', mem)
+  if (mem > 800) {
+    const snapshotDir = path.join(__dirname, '..', 'snapshot')
+    heapdump.writeSnapshot(path.join(snapshotDir, `${Date.now()}.heapsnapshot`))
+    console.log('Stack Overlfow! ')
+    process.exit(1);
+  } else {
+    setCache()
+  }
+}
+
+setCache();
