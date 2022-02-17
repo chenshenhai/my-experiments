00a5601049c77354a4a2064040c9ec481c803bc7
chenshenhai <cshenhai@gmail.com>
2022-02-17 22:55:27 +0800

    feat: update

---
+++ src/system/monitor-server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const Koa = require('koa');
+const koaStatic = require('koa-static');
+
+const app = new Koa();
+
+app.use(koaStatic(path.join(__dirname, 'public-monitor')));
+
+app.listen(3002, () => {
+  console.log('Start: http://127.0.0.1:3002')
+});
+
+process.on('message', (data) => {
+  console.log('[master get data]:', data);
+});
+
--- src/system/monitor.js
+++ src/system/monitor.js
@@ -1,7 +1,13 @@
-function monitor() {
-  console.log('monitor ====')
-}
+const path = require('path');
+const childProcess = require('child_process');
 
+function monitor() {
+  const child = childProcess.fork(path.join(__dirname, 'monitor-server.js'));
+  
+  // setInterval(() => {
+  //   child.send({ hello: 'data from master' });
+  // })
+}
 
 module.exports = {
   monitor,
