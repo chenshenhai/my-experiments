2687dc474bc6ec44c9f19946fa12a27b5a6402b5
chenshenhai <cshenhai@gmail.com>
2022-03-13 15:45:39 +0800

    feat: update

--- src/lib/container.tsx
+++ src/lib/container.tsx
@@ -6,12 +6,16 @@ export const Container: React.FC = () => (
   <div className='page'>
 
     <div className='page-module'>
-      <Box name="hello" />
+      <Box name="box-001" />
+      <Box name="box-002" />
+      <Box name="box-003" />
     </div>
     <div className='page-module'>
-      <Wrapper name="001" />
-      <Wrapper name="002" />
-      <Wrapper name="003"/>
+      <div className='wrapper-list'>
+        <Wrapper name="001" />
+        <Wrapper name="002" />
+        <Wrapper name="003"/>
+      </div>
     </div>
   </div>
 )
